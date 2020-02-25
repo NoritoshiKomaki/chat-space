ed76c4f79206a32bd5cf3f1121c1635fef57b2e6
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-02-25 21:00:28 +0900

    auto-reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,55 @@
 $(function(){
-  function buildHTML(message){
-    if (message.image) {
-      var html =
-      `<div class="message">
-        <div class="list__member">
-          <div class="list__member--left">
-            ${message.user_name}
-          </div>
-          <div class="list__member--right">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="list__text">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-        <image src=${message.image}>
-      </div>`
-    return html;  
-    } else {
-      var html = 
-      `<div class="message">
-        <div class="list__member">
-          <div class="list__member--left">
-            ${message.user_name}
-          </div>
-          <div class="list__member--right">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="list__text">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
-      return html;
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="list__member">` +
+          `<div class="list__member--left">` +
+            message.user_name +
+          `</div>` +
+          `<div class="list__member--right">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="list__text">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="list__member">` +
+          `<div class="list__member--left">` +
+            message.user_name +
+          `</div>` +
+          `<div class="list__member--right">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="list__text">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="list__member">` +
+          `<div class="list__member--left">` +
+            message.user_name +
+          `</div>` +
+          `<div class="list__member--right">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="list__text">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
     };
-  }
+    return html;
+  };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -62,4 +73,29 @@ $(function(){
       alert('メッセージ送信に失敗しました');
     });
   });
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main_chat__list').append(insertHTML);
+        $('.main_chat__list').animate({ scrollTop: $('.main_chat__list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
